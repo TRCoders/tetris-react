7bc6ca9841dd01c2b662cf5fcaeba354b853d93f
Tyler Robinson <tyler.robinson.business@gmail.com>
2022-03-19 04:52:36 -0400

    update

--- src/components/styles/StyledCell.js
+++ src/components/styles/StyledCell.js
@@ -6,8 +6,8 @@ export const StyledCell = styled.div`
     border: ${props => (props.type === 0 ? '0px solid' : '4px solid')};
 
     // Add shadows to the blocks
-    border-bottom-color: rgba(${props = props.color}, 0.1);
-    border-right-color: rgba(${props = props.color}, 1);
-    border-top-color: rgba(${props = props.color}, 1);
-    border-left-color: rgba(${props = props.color}, 0.3);
+    border-bottom-color: rgba(${props => props.color}, 0.1);
+    border-right-color: rgba(${props => props.color}, 1);
+    border-top-color: rgba(${props => props.color}, 1);
+    border-left-color: rgba(${props => props.color}, 0.3);
 `
--- src/components/styles/StyledTetris.js
+++ src/components/styles/StyledTetris.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components'
 
-import bgImage from '../../img/bg.png'
+import bgImage from '../../img/bg.jpg'
 
 // Adds background image
 export const StyledTetrisWrapper = styled.div`
