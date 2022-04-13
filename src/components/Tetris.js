7caa4033640f8d48a5e40935c7fb28ed5cc485c5
Tyler Robinson <tyler.robinson.business@gmail.com>
2022-04-13 14:05:54 -0400

    update

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 
+import { createStage } from '../gameHelpers';
+
 // Styled Components
 import { StyledTetrisWrapper, StyledTetris } from "./styles/StyledTetris";
 
@@ -21,8 +23,44 @@ const Tetris = () => {
 
   console.log('re-render');
 
+  const movePlayer = dir => {
+    updatePlayerPos({ x: dir, y: 0});
+  }
+
+  const startGame = () => {
+    // Reset the stage
+    setStage(createStage());
+    resetPlayer();
+  }
+
+  const drop = () => {
+    updatePlayerPos({ x: 0, y: 1, collided: false })
+  }
+
+  const dropPlayer = () => {
+    drop();
+  }
+
+  // This is the movement for left and right arrow keys
+  // if keyCode equals to 37, move the player left with movePlayer function
+  // same thing happens if it equals to 39 except it moves the player right
+  // if move equals to 40 then the player drops down to the rest of the blocks.
+
+  const move = ({ keyCode }) => {
+    if(!gameOver) {
+      if(keyCode === 37) {
+        movePlayer(-1);
+      } else if (keyCode === 39) {
+        movePlayer(1)
+      } else if (keyCode === 40) {
+        dropPlayer();
+      }
+
+    }
+  }
+
   return (
-    <StyledTetrisWrapper>
+    <StyledTetrisWrapper role="button" tabIndex="0" onKeyDown={e => move(e)}>
       <StyledTetris>
         <Stage stage={stage} />
         <aside>
@@ -35,7 +73,7 @@ const Tetris = () => {
             <Display text="Level" />
           </div>
           )}
-          <StartButton />
+          <StartButton onClick={startGame}/>
         </aside>
       </StyledTetris>
     </StyledTetrisWrapper>
