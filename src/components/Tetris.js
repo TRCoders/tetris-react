cfb463ad58088bbc972babeb4ef60c6f4a5ffb8b
Tyler Robinson <tyler.robinson.business@gmail.com>
2022-05-08 01:08:32 -0400

    Finished rotating function

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -18,15 +18,14 @@ const Tetris = () => {
   const [dropTime, setDropTime] = useState(null);
   const [gameOver, setGameOver] = useState(false);
 
-  const [player, updatePlayerPos, resetPlayer] = usePlayer();
+  const [player, updatePlayerPos, resetPlayer, playerRotate] = usePlayer();
   const [stage, setStage] = useStage(player, resetPlayer);
 
   console.log("re-render");
 
-  const movePlayer = dir => {
-    if (!checkCollision(player, stage, { x: dir, y: 0})) {
+  const movePlayer = (dir) => {
+    if (!checkCollision(player, stage, { x: dir, y: 0 })) {
       updatePlayerPos({ x: dir, y: 0 });
-
     }
   };
 
@@ -39,7 +38,7 @@ const Tetris = () => {
   };
 
   const drop = () => {
-    if (!checkCollision(player, stage, { x: 0, y: 1})) {
+    if (!checkCollision(player, stage, { x: 0, y: 1 })) {
       updatePlayerPos({ x: 0, y: 1, collided: false });
     } else {
       // Game Over
@@ -48,7 +47,7 @@ const Tetris = () => {
         setGameOver(true);
         setDropTime(null);
       }
-      updatePlayerPos({ x: 0, y: 0, collided: true})
+      updatePlayerPos({ x: 0, y: 0, collided: true });
     }
   };
 
@@ -69,12 +68,14 @@ const Tetris = () => {
         movePlayer(1);
       } else if (keyCode === 40) {
         dropPlayer();
+      } else if (keyCode === 38) {
+        playerRotate(stage, 1);
       }
     }
   };
 
   return (
-    <StyledTetrisWrapper role="button" tabIndex="0" onKeyDown={e => move(e)}>
+    <StyledTetrisWrapper role="button" tabIndex="0" onKeyDown={(e) => move(e)}>
       <StyledTetris>
         <Stage stage={stage} />
         <aside>
--- src/hooks/usePlayer.js
+++ src/hooks/usePlayer.js
@@ -1,7 +1,7 @@
-import { useState, useCallback } from "react";
+import { useState, useCallback } from 'react';
 
-import { TETROMINOS, randomTetromino } from "../tetrominos";
-import { STAGE_WIDTH } from "../gameHelpers";
+import { TETROMINOS, randomTetromino } from '../tetrominos';
+import { STAGE_WIDTH, checkCollision } from '../gameHelpers';
 
 export const usePlayer = () => {
   const [player, setPlayer] = useState({
@@ -11,8 +11,34 @@ export const usePlayer = () => {
   });
 
   const rotate = (matrix, dir) => {
-    
-  }
+    // Make the rows to become cols (transpose)
+    const rotatedTetro = matrix.map((_, index) =>
+      matrix.map((col) => col[index])
+    );
+    // Reverse each row to get a rotated matrix
+    if (dir > 0) return rotatedTetro.map((row) => row.reverse());
+    return rotatedTetro.reverse();
+  };
+
+  // This function rotates player blocks clockwise and counter clockwise.
+  const playerRotate = (stage, dir) => {
+    const clonedPlayer = JSON.parse(JSON.stringify(player));
+    clonedPlayer.tetromino = rotate(clonedPlayer.tetromino, dir);
+
+    const pos = clonedPlayer.pos.x;
+    let offset = 1;
+    while (checkCollision(clonedPlayer, stage, { x: 0, y: 0 })) {
+      clonedPlayer.pos.x += offset;
+      offset = -(offset + (offset > 0 ? 1 : -1));
+      if (offset > clonedPlayer.tetromino[0].length) {
+        rotate(clonedPlayer.tetromino, -dir);
+        clonedPlayer.pos.x = pos;
+        return;
+      }
+    }
+
+    setPlayer(clonedPlayer);
+  };
 
   const updatePlayerPos = ({ x, y, collided }) => {
     setPlayer((prev) => ({
@@ -30,5 +56,5 @@ export const usePlayer = () => {
     });
   }, []);
 
-  return [player, updatePlayerPos, resetPlayer];
+  return [player, updatePlayerPos, resetPlayer, playerRotate];
 };
