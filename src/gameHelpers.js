b798512d3fec3e12c1aa45f08aa472657a1b9415
Tyler Robinson <tyler.robinson.business@gmail.com>
2022-05-14 23:40:35 -0400

    update

--- src/gameHelpers.js
+++ src/gameHelpers.js
@@ -4,7 +4,7 @@ export const STAGE_HEIGHT = 20;
 // Creates the tetris board using clear arrays
 export const createStage = () =>
   Array.from(Array(STAGE_HEIGHT), () =>
-    new Array(STAGE_WIDTH).fill([0, "clear"])
+    new Array(STAGE_WIDTH).fill([0, 'clear'])
   );
 
 export const checkCollision = (player, stage, { x: moveX, y: moveY }) => {
@@ -20,7 +20,7 @@ export const checkCollision = (player, stage, { x: moveX, y: moveY }) => {
           !stage[y + player.pos.y + moveY][x + player.pos.x + moveX] ||
           // 4. Check that the cell we're moving to isn't set to clear
           stage[y + player.pos.y + moveY][x + player.pos.x + moveX][1] !==
-            "clear"
+            'clear'
         ) {
           return true;
         }
