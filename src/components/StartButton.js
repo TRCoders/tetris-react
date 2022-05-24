283f36bb35a1a0af26c64bc3cba50b5ecfe15e28
Tyler Robinson <tyler.robinson.business@gmail.com>
2022-05-23 23:45:30 -0400

    update

--- src/components/StartButton.js
+++ src/components/StartButton.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { StyledStartButton } from './styles/StyledStartButton'
 
+// Display Start button. 
 const StartButton = ({ callback }) => (
     <StyledStartButton onClick={callback}>Start Game</StyledStartButton>
 )
