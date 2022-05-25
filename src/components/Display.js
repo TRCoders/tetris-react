43b732a4b9cc12b7f730200562235c61673b7d74
Tyler Robinson <tyler.robinson.business@gmail.com>
2022-05-24 23:55:44 -0400

    update

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { StyledDisplay } from "./styles/StyledDisplay";
 
+// Display gameover text
 const Display = ({ gameOver, text }) => (
   <StyledDisplay gameOver={gameOver}>{text}</StyledDisplay>
 );
